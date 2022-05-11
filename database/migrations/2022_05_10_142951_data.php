b2b2c6f9c215a7898ca202d226c373910e71f5d6
Judith Herlambang <aditalmahdi@gmail.com>
2022-05-11 07:05:50 +0700

    inisiasi backpack

--- database/migrations/2022_05_10_142951_data.php
+++ database/migrations/2022_05_10_142951_data.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('username');
             $table->string('slug');
-            $table->string('avatar');
+            $table->binary('avatar');
             $table->enum('gender', ['men', 'woman']);
             $table->timestamps();
         });
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->increments('id');
             $table->string('title_tourist');
             $table->text('desct_tourist');
-            $table->string('image_tourist');
+            $table->binary('image_tourist');
             $table->string('slug');
             $table->timestamps();
         });
@@ -38,7 +38,7 @@ return new class extends Migration
             $table->foreign('user_blogs_id')->references('id')->on('user_blogs');
             $table->string('title_articles');
             $table->string('slug');
-            $table->string('image_articles');
+            $table->binary('image_articles');
             $table->text('desc_articles');
             $table->timestamps();
         });
@@ -47,7 +47,7 @@ return new class extends Migration
             $table->increments('id');
             $table->string('about_title');
             $table->text('desc_about');
-            $table->string('image_about');
+            $table->binary('image_about');
             $table->timestamps('');
         });
     }
@@ -62,6 +62,6 @@ return new class extends Migration
         Schema::dropIfExists('articles');
         Schema::dropIfExists('about');
         Schema::dropIfExists('homeTourist');
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_blogs');
     }
 };
